4e98939a122317f50482e6be6ebff82a144a0ed7
Sridhar G K <gksridhar09@gmail.com>
2020-04-30 10:41:52 +0530

    model changes for table relationships

--- app/ProductCategory.php
+++ app/ProductCategory.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductCategory extends Model
@@ -14,4 +15,9 @@ class ProductCategory extends Model
     protected $fillable = [
         'name', 'description', 'image',
     ];
+
+    public function product()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
--- app/ProductPrice.php
+++ app/ProductPrice.php
@@ -2,16 +2,22 @@
 
 namespace App;
 
+use App\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductPrice extends Model
 {
-     /**
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'product_id', 'size', 'description', 'price',
+        'size', 'description', 'price',
     ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
